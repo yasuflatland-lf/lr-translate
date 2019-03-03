322d864c4ea7331849f8047c8ad0a3ad07df531e
Yasuyuki Takeo <yasuyuki.takeo@liferay.com>
2019-03-03 13:52:32 +0900

    Initial layout fixed

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,22 +3,23 @@
     <el-container>
       <el-menu default-active="1" :collapse="isCollapse">
         <el-menu-item index="1">
-          <i class="el-icon-upload"></i>
-          <span slot="title">Navigator Four</span>
+          <i class="el-icon-upload" ></i>
         </el-menu-item>
       </el-menu>
 
       <el-container>
         <el-main>
-          <el-form :inline="true" :model="formInline" class="demo-form-inline">
-            <el-form-item>
-              <el-input v-model="formInline.user" placeholder="Source Folder"></el-input>
+          <el-form :model="formInline" label-width="120px">
+            <el-row style="margin-bottom:24px;padding-bottom:24px;border-bottom: solid 1px #ddd;">
+              <el-button-group >
+                <el-button type="danger" icon="el-icon-circle-check" @click="onSubmit" >Run</el-button>
+              </el-button-group>
+            </el-row>
+            <el-form-item label="Source Folder" >
+              <el-input v-model="formInline.user" placeholder="Translated Folder" ></el-input>
             </el-form-item>
-            <el-form-item >
+            <el-form-item label="Translated Folder" style="margin-bottom:24px;padding-bottom:24px;border-bottom: solid 1px #ddd;">
               <el-input v-model="formInline.user" placeholder="Translated Folder"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="onSubmit">Run</el-button>
             </el-form-item>
           </el-form>        
           <el-table :data="tableData">
@@ -49,6 +50,7 @@
         address: 'No. 189, Grove St, Los Angeles'
       }
       return {
+        labelPosition: 'left',
         isCollapse: true,
         tableData: Array(20).fill(item),
         formInline: {
